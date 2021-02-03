9a9853405cf80bab9bd357735d9b1ad965fb4cdf
XaviiB <xavybiggs@gmail.com>
2021-02-03 11:48:42 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
+  // function to calculate interest rate
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var text;
+
+     p = document.getElementById("principal").value;
+
+     y = document.getElementById("years").value;
+
+     r = document.getElementById("rate").value;
+
+     i = p * y * r / 100;
+
+     var a = "If you deposit " + p + ","+"<br>" +
+             "at an interest rate of " + r + "%," + "<br>" +
+             "You will receive an amount of " + i + "," +"<br>" +
+             "in the year " + y;
+
+             //Validation
+     if (p <= 0) {
+        alert("Enter a positive number")
+      } else {
+     result.innerHTML = a;
+      }
+
 }
-        
+
+// function to see number for slider
+function read(){
+    var range = document.getElementById("rate").value;
+    var output = document.getElementById("display");
+    output.innerHTML = range+"%";
+    }

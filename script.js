0a7d878f20583925d8f8a478d9c1904fe37b5713
XaviiB <xavybiggs@gmail.com>
2021-02-03 19:32:32 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
   // function to calculate interest rate
 function compute()
 {
-    var text;
+
+
 
      p = document.getElementById("principal").value;
 
      y = document.getElementById("years").value;
 
+     ny = document.getElementById("years").value;
+
      r = document.getElementById("rate").value;
+
+     var date = new Date();
+     var d = (date.getFullYear() + document.getElementById("years").value);
 
      i = p * y * r / 100;
 
-     var a = "If you deposit " + p + ","+"<br>" +
+
+   var a = "If you deposit " + p + ","+"<br>" +
              "at an interest rate of " + r + "%," + "<br>" +
              "You will receive an amount of " + i + "," +"<br>" +
-             "in the year " + y;
+             "in the year " + d;
 
              //Validation
      if (p <= 0) {
